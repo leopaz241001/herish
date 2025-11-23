798b6fd4b1b18bfea44f7b000f7fb523a464b805
leopaz241001 <leopaz241001@gmail.com>
2025-11-23 21:13:07 +0700

    update recruitment

--- assets/js/recruitment.js
+++ assets/js/recruitment.js
@@ -1,4 +1,8 @@
 function renderRecruitment(item) {
+  const dateInput = item.deadline;
+  const [day, month, year] = dateInput.split("-");
+  const date = `${year}/${month}/${day}`;
+
   return `
     <li class="recruitment-item grid sm:grid-cols-6 items-center sm:gap-6 gap-4 py-8 max-sm:py-5 border-b border-neutral-hover duration-500 hover:px-8 hover:bg-white hover:bg-opacity-20">
       <div class="lg:col-span-2 col-span-12">
@@ -8,15 +12,15 @@ function renderRecruitment(item) {
         <div class="flex max-sm:flex-wrap sm:gap-6 gap-4">
           <div class="flex items-center gap-3 lg:w-1/3">
             <span class="icon-clock text-[24px]"></span>
-            <span>31/10/2025</span>
+            <span>${date}</span>
           </div>
           <div class="flex items-center gap-3 lg:w-1/3">
             <span class="icon-dollar-circle text-[24px]"></span>
-            <span>up to 20M</span>
+            <span>${item.salary}</span>
           </div>
           <div class="flex items-center gap-3 lg:w-1/3">
             <span class="icon-manager text-[24px]"></span>
-            <span>Middle</span>
+            <span>${item.level}</span>
           </div>
         </div>
       </div>
@@ -32,8 +36,6 @@ async function fetchRecruitment() {
     const res = await fetch('https://herish.id.vn/api/recruitments');
     const data = await res.json();
     const list = data.data;
-    console.log(list);
-    
     
     $("#recruitmentList").html("");
     if(list?.length > 0) {
@@ -55,12 +57,11 @@ async function renderRecruitmentDetail() {
   try {
     let res = await fetch(`https://herish.id.vn/api/recruitments/${recruitment_id}`)
     const data = await res.json();
-    console.log(data);
     if (res.ok) {
       const detail = data.data;
       $(".recruitment-title").text(detail.title);
       $(".recruitment-detail .thumbnail img").attr("src", detail.image_url);
-      
+      $(".recruitment-location").text(detail.location);
     }
   } catch (err) {
     console.error("Fetch recruitments failed:", err);
